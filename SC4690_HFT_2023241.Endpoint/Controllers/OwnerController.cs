d7222757525234b7ebe870bee968c26d83e47209
GazdagDominika <gazdagdominikazoe@gmail.com>
2024-04-24 18:10:34 +0200

    OwnerController changed

--- SC4690_HFT_2023241.Endpoint/Controllers/OwnerController.cs
+++ SC4690_HFT_2023241.Endpoint/Controllers/OwnerController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SC4690_HFT_2023241.Endpoint.Services;
 using SC4690_HFT_2023241.Logic.Interfaces;
 using SC4690_HFT_2023241.Models;
 using System.Collections.Generic;
@@ -11,10 +13,13 @@ namespace SC4690_HFT_2023241.Endpoint.Controllers
     public class OwnerController : ControllerBase
     {
         IOwnerLogic logic;
+        IHubContext<SignalRHub> hub;
 
-        public OwnerController(IOwnerLogic logic)
+        public OwnerController(IOwnerLogic logic, IHubContext<SignalRHub> hub)
         {
             this.logic = logic;
+            this.hub = hub;
+            
         }
 
 
@@ -35,18 +40,25 @@ namespace SC4690_HFT_2023241.Endpoint.Controllers
         public void Create([FromBody] Owner value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("OwnerCreated", value);
+
         }
 
         [HttpPut]
         public void Update( [FromBody] Owner value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("OwnerUpdated", value);
+
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var ownerToDelete = this.logic.Read(id);
+
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("OwnerDeleted", ownerToDelete);
         }
     }
 }
